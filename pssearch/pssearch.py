09c79b21e52687a021dfef69a97619255d7ead03
tommejas <tommejas@gmail.com>
2019-02-04 20:24:39 -0500

    Adding player search via Daybreak API

--- pssearch/pssearch.py
+++ pssearch/pssearch.py
@@ -1,10 +1,18 @@
+import json
+import urllib.request
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     if 'user' in request.args: 
-        return render_template('index.html', user=request.args['user'])
+        search = urllib.request.urlopen("http://census.daybreakgames.com/s:supafarma/get/ps2/character/?name.first_lower={}".format(request.args['user']))
+        search = json.loads(search.read())
+        if search['returned']:
+            search = search['character_list'][0]
+            user = search['name']['first']
+            character_id = search['character_id']
+            return render_template('index.html', user=user, character_id=character_id)
 
     return render_template('index.html')
 
