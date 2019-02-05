1ef5d8811b61b33265c431c13d482cabf0e7e140
tommejas <tommejas@gmail.com>
2019-02-04 21:21:34 -0500

    bug fix

--- pssearch/pssearch.py
+++ pssearch/pssearch.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     if 'user' in request.args: 
-        search = urllib.request.urlopen("http://census.daybreakgames.com/s:supafarma/get/ps2/character/?name.first_lower={}".format(request.args['user']))
+        search = urllib.request.urlopen("http://census.daybreakgames.com/s:supafarma/get/ps2/character/?name.first_lower={}".format(request.args['user'].lower()))
         search = json.loads(search.read())
         if search['returned']:
             search = search['character_list'][0]
             user = search['name']['first']
             character_id = search['character_id']
             return render_template('index.html', user=user, character_id=character_id)
-
+        return render_template('index.html', status='usernotfound')
     return render_template('index.html')
 
