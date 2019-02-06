28ded89b09b57294ff627e3f95598271d895a5d2
tommejas <tommejas@gmail.com>
2019-02-06 12:02:10 -0500

    Function for character search created

--- pssearch/pssearch.py
+++ pssearch/pssearch.py
@@ -3,16 +3,26 @@ import urllib.request
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+def get_character(name):
+    character_id_url = "http://census.daybreakgames.com/s:supafarma/get/ps2/character/?name.first_lower={}".format(name.lower())
+    character_id_result = urllib.request.urlopen(character_id_url)
+    character_id_result = json.loads(character_id_result.read())
+    if character_id_result['returned']:
+        character_id_result = character_id_result['character_list'][0]
+        username = character_id_result['name']['first']
+        character_id = character_id_result['character_id']
+        return [username, character_id]
+    return False
+
+
 @app.route('/')
 def index():
-    if 'user' in request.args: 
-        search = urllib.request.urlopen("http://census.daybreakgames.com/s:supafarma/get/ps2/character/?name.first_lower={}".format(request.args['user'].lower()))
-        search = json.loads(search.read())
-        if search['returned']:
-            search = search['character_list'][0]
-            user = search['name']['first']
-            character_id = search['character_id']
-            return render_template('index.html', user=user, character_id=character_id)
-        return render_template('index.html', status='usernotfound')
+    if 'user' in request.args:
+        character_results = get_character(request.args['user'])
+        if character_results:
+            return render_template('index.html', user=character_results[0], character_id=character_results[1])
+        else:
+            return render_template('index.html', status="usernotfound")
+
     return render_template('index.html')
 
