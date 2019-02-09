80adca687c9cf9dc6a800a02debcee9da6314671
tommejas <tommejas@gmail.com>
2019-02-08 22:12:05 -0500

    VERY MESSY, added accuracy for infantry guns of player searched

--- pssearch/pssearch.py
+++ pssearch/pssearch.py
@@ -1,5 +1,4 @@
 import json
-import os
 import pickle
 import urllib.request
 from flask import Flask, render_template, request
@@ -20,6 +19,11 @@ def get_character(name):
         return [username, character_id]
     return False
 
+def get_name_from_id(item_id):
+    if item_id in item_names:
+        return item_names[item_id]
+    return "notinlist"
+
 def get_weapon_accuracy(character_id, item_id):
     weapon_url = "http://census.daybreakgames.com/s:supafarma/get/ps2/characters_weapon_stat/?character_id={}&item_id={}&stat_name=weapon_fire_count,weapon_hit_count&c:limit=2&c:show=value".format(character_id, item_id)
     weapon_result = urllib.request.urlopen(weapon_url).read()
@@ -27,10 +31,23 @@ def get_weapon_accuracy(character_id, item_id):
     weapon_result = int(weapon_result['characters_weapon_stat_list'][0]['value']) / int(weapon_result['characters_weapon_stat_list'][1]['value'])
     return weapon_result
 
-def test(name):
-    all_wep_stats = "http://census.daybreakgames.com/get/ps2/characters_weapon_stat/?character_id={}&stat_name=weapon_fire_count,weapon_hit_count&vehicle_id=0&c:show=stat_name,item_id,value,last_save_date&c:limit=8000".format(name.lower())
+def get_all_weapon_accuracy(name):
+    all_wep_stats = "http://census.daybreakgames.com/get/ps2/characters_weapon_stat/?character_id={}&stat_name=weapon_fire_count,weapon_hit_count&vehicle_id=0&c:show=stat_name,item_id,value&c:limit=500".format(name.lower())
     all_wep_res = json.loads(urllib.request.urlopen(all_wep_stats).read())
-    print(all_wep_res)
+    all_wep_res = all_wep_res['characters_weapon_stat_list']
+    all_gun_acc = {}
+    for value in all_wep_res:
+        if value['stat_name'] == 'weapon_fire_count':
+            all_gun_acc[value['item_id']] = [value['value']]
+        else:
+            if value['item_id'] in all_gun_acc:
+                all_gun_acc[value['item_id']].append(value['value'])
+    xd = {}
+    for key, value in all_gun_acc.items():
+        if len(value) == 2:
+            xd[get_name_from_id(key)] = int(value[1])/int(value[0])
+            
+    return xd
 
 @app.route('/')
 def index():
@@ -38,9 +55,8 @@ def index():
         character_results = get_character(request.args['user'])
         if character_results:
             user, character_id = character_results
-            test(character_id)
-            print(os.getcwd())
-            return render_template('index.html', user=user, character_id=character_id)
+            get_all_weapon_accuracy(character_id)
+            return render_template('index.html', user=user, character_id=character_id, gun_names=get_all_weapon_accuracy(character_id))
         else:
             return render_template('index.html', status="usernotfound")
     return render_template('index.html', gun_names=item_names)
