47a17971fd3055a75ce95af137b7da4fc0a60ed7
tommejas <tommejas@gmail.com>
2019-02-17 14:24:39 -0500

    Complete overhual of the searching, much more efficient, module created for the searching, can search with gun list (kills,deaths,hs,acc,name) or character alone

---
+++ pssearch/char_search.py
@@ -0,0 +1,111 @@
+import urllib.request, json
+
+url = """http://census.daybreakgames.com/s:supafarma/get/ps2/character/?name.first_lower={}&c:show=character_id,name.first,faction_id,times.minutes_played,battle_rank.value,prestige_level&c:join=faction^inject_at:faction_id^show:code_tag&c:join=characters_weapon_stat^inject_at:wep_acc^terms:vehicle_id=0%27stat_name=weapon_deaths%27stat_name=weapon_fire_count%27stat_name=weapon_hit_count^show:item_id%27stat_name%27value^list:1^outer:0(item^show:name.en^inject_at:gun_name)&c:tree=start:wep_acc^field:item_id,stat_name^list:1&c:join=characters_weapon_stat_by_faction^inject_at:wep_kill^terms:vehicle_id=0%27stat_name=weapon_headshots%27stat_name=weapon_kills^show:item_id%27stat_name%27value_vs%27value_nc%27value_tr^list:1(item^show:name.en^inject_at:gun_name)&c:tree=start:wep_kill^field:item_id,stat_name^list:1"""
+
+"""
+u = url
+j = json
+r = access data
+k = weapon kills, weapon headshots, located where injected
+a = weapon accuracy, deaths, location where injected
+f = final dict, remove incomplete items(must have all 5 stats, weapon: kills, deaths, hit, fired, headshots)
+g = gun list with 
+
+create character with stuff and gun list
+"""
+
+class Character:
+    def __init__(self, cid, name, fac, time, rank, pres):
+        self.cid = cid
+        self.name = name
+        self.fac = fac
+        self.time = time
+        self.rank = rank
+        self.pres = pres
+
+    def set_guns(self, guns):
+        self.guns = guns
+
+class Gun:
+    def __init__(self, d):
+        self.d = d
+        self.item_id = list(d.keys())[0]
+        self.name = d['name']
+        self.kills = d['weapon_kills']
+        self.headshots = d['weapon_headshots']
+        self.deaths = d['weapon_deaths']
+        self.fired = d['weapon_fire_count']
+        self.landed = d['weapon_hit_count']
+
+def get_char(name):
+    u = urllib.request.urlopen(url.format(name.lower()))
+    j = json.load(u)
+    if not j['returned']:
+        return False
+    r = j['character_list'][0]
+
+    c = Character(
+            r['character_id'],
+            r['name']['first'],
+            r['faction_id']['code_tag'],
+            r['times']['minutes_played'],
+            r['battle_rank']['value'],
+            r['prestige_level']
+            )
+
+    return c
+
+
+def get_char_with_guns(name):
+    u = urllib.request.urlopen(url.format(name.lower()))
+    j = json.load(u)
+
+    if not j['returned']:
+        return False
+
+    r = j['character_list'][0]
+    k = r['wep_kill']
+    a = r['wep_acc']
+
+    c = Character(
+            r['character_id'],
+            r['name']['first'],
+            r['faction_id']['code_tag'],
+            r['times']['minutes_played'],
+            r['battle_rank']['value'],
+            r['prestige_level']
+            )
+
+    f = {}
+
+    for item_id, values in k.items():
+        if not item_id in f:
+            f[item_id] = {}
+        for stat_name, result in values.items():
+            final_val = 0
+            for i, v in result[0].items():
+                if isinstance(v, str):
+                    final_val = final_val + int(v)
+            if not 'name' in f[item_id]:
+                f[item_id]['name'] = result[0]['gun_name']['name']['en']
+            f[item_id].update({stat_name: final_val})
+
+    for item_id, values in a.items():
+        if not item_id in f:
+            f[item_id] = {}
+        for stat_name, result in values.items():
+            if not 'name' in f[item_id]:
+                f[item_id]['name'] = result[0]['gun_name']['name']['en']
+            f[item_id].update({stat_name: result[0]['value']})
+
+    for i, v in f.copy().items():
+        if not len(v) == 6:
+            del f[i]
+
+    g = []
+
+    for i, v in f.items():
+        g.append(Gun(v))
+
+    c.set_guns(g)
+    return c
--- pssearch/pssearch.py
+++ pssearch/pssearch.py
@@ -2,45 +2,10 @@ import urllib.request, json, sqlite3, os
 from flask import Flask, render_template, request, session, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 
+import char_search
+
 app = Flask(__name__)
 app.secret_key = "a"
-
-class Character:
-    def __init__(self, c_id, name, fac):
-        self.c_id = c_id
-        self.name = name
-        self.fac = fac
-
-def url_json(url):
-    url_res = urllib.request.urlopen(url).read()
-    json_res = json.loads(url_res)
-    if json_res['returned']:
-        return json_res
-    else:
-        return False
-
-def search_character(name):
-    url = "http://census.daybreakgames.com/s:supafarma/get/ps2/character/?name.first_lower={}&c:join=faction^show:code_tag".format(name.lower())
-    json_res = url_json(url)
-    if json_res:
-        char_json = json_res['character_list'][0]
-        return Character(char_json['character_id'], char_json['name']['first'], char_json['faction_id_join_faction']['code_tag'])
-    
-def get_all_wep_acc(char_id):
-    url = "http://census.daybreakgames.com/s:supafarma/get/ps2/characters_weapon_stat/?character_id={}&stat_name=weapon_fire_count,weapon_hit_count&vehicle_id=0&c:show=stat_name,item_id,value&c:limit=500&c:join=item^show:name.en".format(char_id)
-    json_res = url_json(url)
-    if json_res:
-        wep_json = json_res['characters_weapon_stat_list']
-        wep_dict = {}
-        for wep in wep_json:
-            item_id = wep['item_id']
-            if not item_id in wep_dict:
-                wep_dict[item_id] = [wep['item_id_join_item']['name']['en']]
-            wep_dict[item_id].append(wep['value'])
-        for key, value in list(wep_dict.items()):
-            if len(value) != 3:
-                del wep_dict[key]
-        return wep_dict
 
 def create_account(username, password):
     con = sqlite3.connect(os.path.join(app.root_path, 'static/accounts.db'))
@@ -73,9 +38,9 @@ def index():
 @app.route('/character/')
 def character():
     if 'name' in request.args:
-        char_search = search_character(request.args.get('name'))
-        if char_search:
-            return render_template('character.html', char=char_search)
+        char = char_search.get_char_with_guns(request.args['name'])
+        if char:
+            return render_template('character.html', char=char)
     return render_template('character.html')
 
 @app.route('/account/', methods=['GET'])
@@ -85,39 +50,16 @@ def account_get():
 @app.route('/account/', methods=['POST'])
 def account_post():
     if 'sign-up' in request.form:
-        create_account(request.form.get('sign-up-user'), request.form.get('sign-up-password'))
-        session.update(user = request.form.get('sign-up-user'))
+        try:
+            create_account(request.form.get('sign-up-user'), request.form.get('sign-up-password'))
+            session.update(user = request.form.get('sign-up-user'))
+        except sqlite3.IntegrityError:
+            return 'nametaken'
     if 'log-in' in request.form:
         if check_account(request.form.get('log-in-user'), request.form.get('log-in-password')):
             session.update(user = request.form.get('log-in-user'))
+        else:
+            return 'loginfail'
     if 'log-out' in request.form:
         session.clear()
     return render_template('account.html')
-
-
-'''
-
-@app.route('/', methods=['POST'])
-def index_post():
-    if check_account(request.form.get('login-user'), request.form.get('login-password')):
-        session.update(user = ((request.form.get('login-user')), request.form.get('login-password')))
-        return redirect(request.referrer)
-    else:
-        return render_template('index.html', status='accountnotfoundorwrongpassword')
-
-# temp
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.clear()
-    return redirect(request.referrer)
-
-@app.route('/signup', methods=['GET'])
-def signup_get():
-    return render_template('signup.html')
-
-@app.route('/signup', methods=['POST'])
-def signup_post():
-    create_account(request.form['user'], request.form['password'])
-    return render_template('index.html')
-
-'''
